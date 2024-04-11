b8c60347557d351896eaacc17d8690207670da7a
xintaonian <johnnian@hotmail.com>
2024-04-11 20:50:24 +1000

    python lesson Sum of square diff

---
+++ Pseudocode/Sum Square Difference.py
@@ -0,0 +1,21 @@
+# initialise sum_of_squares = 0
+sum_of_squares = 0
+# initialise sum = 0
+sum = 0
+# initialise i = 1
+i = 1
+
+# while i <= 100
+while i <= 100:
+    #     sum_of_squares = sum _of_squares =i *i
+    sum_of_squares = sum_of_squares + i * i
+    #     sum = sum + i
+    sum = sum + i
+    #     i = i+1
+    i = i + 1
+     #   square_of_some = sum * sum
+    square_of_sum = sum * sum
+    # diff =
+    diff = square_of_sum-sum_of_squares
+print(diff )
+
