8e53e73efdd8f6056a62377d446d69948b600136
xintaonian <johnnian@hotmail.com>
2024-04-11 21:14:41 +1000

    python lesson Sum of square diff

--- loops.py
+++
@@ -1,9 +0,0 @@
-number =1
-
-# 当数值小于等于5时 显示数字 > 数值+1 > 再次检测数值是否小于等于5 > 直至大于5 > 显示end
-while number <= 5:
-    print(number)
-    number=number + 1
-print("end")
-
-
--- ternary_operator.py
+++
@@ -1,2 +0,0 @@
-is_raining = input("Is it raining ? Yes or No: ")
-print("Yes I need an umbrella") if is_raining == "Yes" else print("No you don't need an umbrella")
